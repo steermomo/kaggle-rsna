64e27fe29e2a0c4d5620150584db50c95985a399
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-08 06:38:08 +0000

    fix bug in extract

--- 00_1_data_download_extrach.sh
+++ 00_1_data_download_extrach.sh
@@ -5,7 +5,7 @@ EXTRACT_DIR=/home/jupyter/rsna/source_data
 
 #https://stackoverflow.com/questions/59838/check-if-a-directory-exists-in-a-shell-script
 if [ ! -d "$DOWNLOAD_DIR" ]; then
-  mkdir $DOWNLOAD_DIR && echo 'mkdir $DOWNLOAD_DIR'
+  mkdir -p $DOWNLOAD_DIR && echo 'mkdir $DOWNLOAD_DIR'
 else
     echo ${DOWNLOAD_DIR}" exists"
 fi
@@ -13,6 +13,14 @@ fi
 cd $DOWNLOAD_DIR && echo "cd "$DOWNLOAD_DIR
 
 
-kaggle competitions download -c rsna-intracranial-hemorrhage-detection
+kaggle competitions download -c rsna-intracranial-hemorrhage-detection -o
 
-unzip $DOWNLOAD_DIR/rsna-intracranial-hemorrhage-detection.zip -d $EXTRACT_DIR -q
+
+if [ ! -d "$EXTRACT_DIR" ]; then
+  mkdir -p $EXTRACT_DIR && echo 'mkdir $EXTRACT_DIR'
+else
+    echo ${EXTRACT_DIR}" exists"
+fi
+
+echo '=> Start extract to '$EXTRACT_DIR
+unzip -qo $DOWNLOAD_DIR/rsna-intracranial-hemorrhage-detection.zip -d $EXTRACT_DIR 
